81f1431a4027810a0244473d78c80b04dc2ca54e
Marcos <plimo263@gmail.com>
2020-07-06 16:34:20 -0300

    FAZENDO COM QUE O MES TENHA UM CAMPO TODOS NO PADRAO-FORM

--- src/components/padrao-form.js
+++ src/components/padrao-form.js
@@ -308,7 +308,8 @@ class PadraoForm extends React.Component {
         let {mes} = this.props;
         // Agora gera os anos e os meses        
         let meses = [ [1, "JAN"], [2, "FEV"], [3, "MAR"], [4, "ABR"], [5, "MAI"], [6, "JUN"],
-            [7, "JUL"], [8, "AGO"], [9, "SET"], [10, "OUT"], [11, "NOV"], [12, "DEZ"] ];
+            [7, "JUL"], [8, "AGO"], [9, "SET"], [10, "OUT"], [11, "NOV"], [12, "DEZ"],
+            ["1,2,3,4,5,6,7,8,9,10,11,12", "TODOS"] ];
         
         let filtro = meses.filter(ele=> ele[0].toString() === mes.toString()).map(ele=> ele[0].toString()).join(',');
         filtro = multiplo ? this._obterDefault(filtro, meses) : this._obterDefault(filtro, meses)[0];
